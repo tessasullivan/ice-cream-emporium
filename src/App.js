65c1a075810c7c8a31275916bdce07e0968ca388
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-07-12 11:42:11 -0700

    Missed one file rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
-import IceCream from "./components/iceCreamMain/iceCream";
+import IceCream from './components/iceCreamMain/iceCream';
 import IceCreamForm from "./components/iceCreamForm/iceCreamForm";
 import NavBar from "./components/navBar/navBar";
 import Employees from "./components/employees/employees";
